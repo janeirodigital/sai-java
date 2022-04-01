4dcd0850d1ddcf73f30b0b68908e1cb19c403afc
Justin Bingham <justin.bingham@janeirodigital.com>
2022-04-01 09:10:19 -0400

    update package information

--- sai-java-core/src/main/java/com/janeirodigital/sai/core/agents/package-info.java
+++ sai-java-core/src/main/java/com/janeirodigital/sai/core/agents/package-info.java
@@ -1,5 +1,8 @@
 /**
- * Access to corresponding RDF Resources providing create, read, update
- * and delete capabilities
+ * Classes providing access to
+ * <a href= "https://solid.github.io/data-interoperability-panel/specification/#ar-registry">Agent Registries</a>,
+ * <a href= "https://solid.github.io/data-interoperability-panel/specification/#social-agents">Social Agent Profiles</a>,
+ * <a href= "https://solid.github.io/data-interoperability-panel/specification/#app">Application Profiles</a>,
+ * and <a href= "https://solid.github.io/data-interoperability-panel/specification/#ar">Agent Registrations</a>.
  */
 package com.janeirodigital.sai.core.agents;
--- sai-java-core/src/main/java/com/janeirodigital/sai/core/data/package-info.java
+++ sai-java-core/src/main/java/com/janeirodigital/sai/core/data/package-info.java
@@ -1,4 +1,7 @@
 /**
- * Access to corresponding RDF Resources providing read-only capabilities
+ * Classes providing access to
+ * <a href="https://solid.github.io/data-interoperability-panel/specification/#data-instance">Data Instances</a>,
+ * <a href="https://solid.github.io/data-interoperability-panel/specification/#data-registration">Data Registrations</a>,
+ * and <a href="https://solid.github.io/data-interoperability-panel/specification/#data-registry">Data Registries</a>.
  */
 package com.janeirodigital.sai.core.data;
